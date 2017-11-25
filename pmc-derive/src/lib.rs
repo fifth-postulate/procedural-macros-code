738731a42eaac56f9f5c0b9dc1a8460580cca294
Daan van Berkel <daan.v.berkel.1980@gmail.com>
2017-11-25 15:05:40 +0100

    Derive an Drop implementation

--- pmc-derive/src/lib.rs
+++ pmc-derive/src/lib.rs
@@ -9,10 +9,13 @@ pub fn announce_drop(input: TokenStream) -> TokenStream {
     let source = input.to_string();
 
     let ast = syn::parse_derive_input(&source).unwrap();
+    let name = &ast.ident;
 
-    empty_token_stream()
-}
-
-fn empty_token_stream() -> TokenStream {
-    "".parse().unwrap()
+    quote!(
+        impl Drop for #name {
+            fn drop(&mut self) {
+                println!("#name dropped");
+            }
+        }
+    ).parse().unwrap()
 }
