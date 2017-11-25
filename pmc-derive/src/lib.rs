f328f24bf3c97bac8b46502e7c0fa87e8f279d02
Daan van Berkel <daan.v.berkel.1980@gmail.com>
2017-11-25 11:16:54 +0100

    Parse source with syn

--- pmc-derive/src/lib.rs
+++ pmc-derive/src/lib.rs
@@ -6,7 +6,9 @@ use proc_macro::TokenStream;
 
 #[proc_macro_derive(AnnounceDrop)]
 pub fn announce_drop(input: TokenStream) -> TokenStream {
-    println!("Deriving Drop");
+    let source = input.to_string();
+
+    let ast = syn::parse_derive_input(&source).unwrap();
 
     empty_token_stream()
 }
