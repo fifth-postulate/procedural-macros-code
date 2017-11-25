e0e9029c0f544e39c332eadba34615f98cb4f60b
Daan van Berkel <daan.v.berkel.1980@gmail.com>
2017-11-25 08:03:36 +0100

    Create a skeletal custom derive

--- pmc-derive/src/lib.rs
+++ pmc-derive/src/lib.rs
@@ -1,7 +1,16 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
-    }
+extern crate proc_macro;
+extern crate syn;
+#[macro_use] extern crate quote;
+
+use proc_macro::TokenStream;
+
+#[proc_macro_derive(AnnounceDrop)]
+pub fn announce_drop(input: TokenStream) -> TokenStream {
+    println!("Deriving Drop");
+
+    empty_token_stream()
+}
+
+fn empty_token_stream() -> TokenStream {
+    "".parse().unwrap()
 }
