50d465c10391e9530ab8a040d72328b5c8af5dde
Daan van Berkel <daan.v.berkel.1980@gmail.com>
2017-11-28 10:44:45 +0100

    Parse Rust source

--- pmc-derive/src/lib.rs
+++ pmc-derive/src/lib.rs
@@ -1,12 +1,17 @@
 extern crate proc_macro;
 extern crate syn;
-#[macro_use] extern crate quote;
 
 use proc_macro::TokenStream;
 
 #[proc_macro_derive(AnnounceDrop)]
-pub fn announce_drop(_input: TokenStream) -> TokenStream {
-    println!("I print during the compilation phase");
+pub fn announce_drop(input: TokenStream) -> TokenStream {
+    let source = input.to_string();
+
+    let ast = syn::parse_derive_input(&source).unwrap();
+    let name = &ast.ident;
+    let call_name = name.to_string();
+
+    println!("Deriving for {}\n{:?}", call_name, ast);
 
     "".parse().unwrap()
 }
